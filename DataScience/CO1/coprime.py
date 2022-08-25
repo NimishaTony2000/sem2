a630ef76039c86edd39db71f50b5189958dfbe45
NimishaTony2000 <95517102+NimishaTony2000@users.noreply.github.com>
2022-08-25 15:57:59 +0530

    Add files via upload

---
+++ DataScience/CO1/coprime.py
@@ -0,0 +1,12 @@
+print("21MCA033-Nimisha Tony")
+
+import math
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+print("gcd of numbers")
+num3 = math.gcd(num1, num2)
+print(num3)
+if num3 == 1:
+    print("Given numbers are coprime")
+else:
+    print("Given numbers are not coprime")
